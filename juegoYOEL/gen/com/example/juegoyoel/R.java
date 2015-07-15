b867af64190d4f85af39fb53715880c81e917083
yoelcesar <yoelruiztaipe@gmail.com>
2015-07-15 02:36:01 -0500

    comit del fondo de pantalla de mi juego

--- juegoYOEL/gen/com/example/juegoyoel/R.java
+++ juegoYOEL/gen/com/example/juegoyoel/R.java
@@ -1926,8 +1926,9 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020038;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020039;
         public static final int abc_textfield_search_material=0x7f02003a;
-        public static final int ic_launcher=0x7f02003b;
-        public static final int notification_template_icon_bg=0x7f02003c;
+        public static final int fondo=0x7f02003b;
+        public static final int ic_launcher=0x7f02003c;
+        public static final int notification_template_icon_bg=0x7f02003d;
     }
     public static final class id {
         public static final int action0=0x7f09004f;
