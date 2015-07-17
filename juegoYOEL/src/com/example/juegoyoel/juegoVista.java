74348a05a552f924c0817d723579db0769d5165d
yoelcesar <yoelruiztaipe@gmail.com>
2015-07-16 21:55:52 -0500

    arregla de interfas del juegoa adaptarse a  cualquier pantalla

--- juegoYOEL/src/com/example/juegoyoel/juegoVista.java
+++ juegoYOEL/src/com/example/juegoyoel/juegoVista.java
@@ -1,11 +1,36 @@
 package com.example.juegoyoel;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class juegoVista extends ActionBarActivity {
-
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+public class juegoVista extends view implements Runnable{
+	
+	private Bitmap bmpFundo; //declaramos una imagen
+	
+    public void iniciarjuego(){
+	
+	     bmpFundo = BitmapFactory.decodeResource(getResources(), R.drawable.fondo);//cargar la imagen
+	     bmpFundo = Bitmap.createScaledBitmap(bmpFundo, getWidth(), getHeight(), true);// cambiamos las medidas de laimagen de fondo 
+         iniciarjuego = true;
 	}
+    
+private int getHeight() {
+	
+	return 0;
+}
+private int getWidth() {
+	
+	return 0;
+}
+private Resources getResources() {
+	
+		return null;
+}
 }
