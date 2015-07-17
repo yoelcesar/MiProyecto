523ee0caa7f98999c359b730fa5953895402d272
yoelcesar <yoelruiztaipe@gmail.com>
2015-07-16 20:41:10 -0500

    arrglando mi codigo

--- juegoYOEL/src/com/example/juegoyoel/MainActivity.java
+++ juegoYOEL/src/com/example/juegoyoel/MainActivity.java
@@ -13,9 +13,4 @@ public class MainActivity extends Activity {
 		juegoVista game = new juegoVista();
         setContentView(game);
     }
-
-	private void setContentView(juegoVista game) {
-		
-		
-	}
 }
